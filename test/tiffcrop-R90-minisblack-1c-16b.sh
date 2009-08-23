25e5ddd88bbc890624b66f6a8a1efeb7e3115224
Bob Friesenhahn <bfriesen@simple.dallas.tx.us>
2009-08-23 22:58:25 +0000

    tiffcrop.sh into a collection of many specific tests.  Re-wrote all of the existing tests to be based on some simple shell functions.  Make distcheck works again.

--- test/tiffcrop-R90-minisblack-1c-16b.sh
+++ test/tiffcrop-R90-minisblack-1c-16b.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 . ${srcdir:-.}/common.sh
 infile="$srcdir/images/minisblack-1c-16b.tiff"
-outfile="o-tiffcrop-doubleflip-minisblack-1c-16b.tiff"
-f_test_convert "$TIFFCROP -F both" $infile $outfile
+outfile="o-tiffcrop-R90-minisblack-1c-16b.tiff"
+f_test_convert "$TIFFCROP -R90" $infile $outfile
 f_tiffinfo_validate $outfile
--- test/tiffcrop-R90-minisblack-2c-8b-alpha.sh
+++ test/tiffcrop-R90-minisblack-2c-8b-alpha.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 . ${srcdir:-.}/common.sh
 infile="$srcdir/images/minisblack-2c-8b-alpha.tiff"
-outfile="o-tiffcrop-doubleflip-minisblack-2c-8b-alpha.tiff"
-f_test_convert "$TIFFCROP -F both" $infile $outfile
+outfile="o-tiffcrop-R90-minisblack-2c-8b-alpha.tiff"
+f_test_convert "$TIFFCROP -R90" $infile $outfile
 f_tiffinfo_validate $outfile
