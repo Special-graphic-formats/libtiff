Special-graphic-formats/libtiff

commit 02bb01750fd03cdc4a429a02b26a6bdaa250ab4e

tif_fax3.h: allow 0 length run in DECODE2D
fixes #46
https://gitlab.com/libtiff/libtiff/issues/46
http://bugzilla.maptools.org/show_bug.cgi?id=2434

libtiff/tif_fax3.h
@@ -478,11 +478,9 @@ done1d:									\
 	    break;							\
 	case S_VL:							\
 	    CHECK_b1;							\
-	    if (b1 <= (int) (a0 + TabEnt->Param)) {			\
-		if (b1 < (int) (a0 + TabEnt->Param) || pa != thisrun) {	\
-		    unexpected("VL", a0);				\
-		    goto eol2d;						\
-		}							\
+	    if (b1 < (int) (a0 + TabEnt->Param)) {			\
+		unexpected("VL", a0);				\
+		goto eol2d;						\
 	    }								\
 	    SETVALUE(b1 - a0 - TabEnt->Param);				\
 	    b1 -= *--pb;						\

commit 5e75d083b58c417ef610462b59703262d3a32ef6

tif_fax3.h: check for buffer overflow in EXPAND2D before "calling" CLEANUP_RUNS()
fixes #179

this fixes the regression introduced in 02bb0175 / 72c4acef
( merge request !110 )

It may be a better fix to do the overflow check in SETVALUE() but the
macro do { } while(0) construct makes it difficult to quit the loop
properly.

libtiff/tif_fax3.h
@@ -514,7 +514,9 @@ done1d:									\
 	    goto eol2d;							\
 	eof2d:								\
 	    prematureEOF(a0);						\
-	    CLEANUP_RUNS();						\
+		if (pa < thisrun + sp->nruns) {	\
+		    CLEANUP_RUNS();					\
+		}								\
 	    goto eoflab;						\
 	}								\
     }									\